662306eb3502e318922b98c9109b165be90d53c5
Pongsakorn Somsri <pongsakorns@gmail.com>
2019-09-10 01:58:40 +0700

    Test log

--- src/main/java/com/solar/ms/restaurantmanagementsystem/controller/TestController.java
+++ src/main/java/com/solar/ms/restaurantmanagementsystem/controller/TestController.java
@@ -1,14 +1,17 @@
 package com.solar.ms.restaurantmanagementsystem.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestController {
 
     @PostMapping("/v1/test")
     public ResponseEntity<String> testEndpoint(String payload){
+        log.info("{}", payload);
         return ResponseEntity.ok(payload);
     }
 }
