49be80492ccd5428696ea91fb4f8ab5741c959ab
Pongsakorn Somsri <pongsakorns@gmail.com>
2019-09-11 00:42:08 +0700

    Add request body

--- src/main/java/com/solar/ms/restaurantmanagementsystem/controller/TestController.java
+++ src/main/java/com/solar/ms/restaurantmanagementsystem/controller/TestController.java
@@ -3,6 +3,7 @@ package com.solar.ms.restaurantmanagementsystem.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @PostMapping("/v1/test")
-    public ResponseEntity<String> testEndpoint(String payload){
+    public ResponseEntity<String> testEndpoint(@RequestBody String payload){
         log.info("{}", payload);
         return ResponseEntity.ok(payload);
     }
