c6698c867dce5223b502bf2c935ce700dc6cca64
Pongsakorn Somsri <pongsakorns@gmail.com>
2019-09-15 11:00:24 +0700

    Update payment service

--- src/main/java/com/solar/ms/rms/service/PaymentService.java
+++ src/main/java/com/solar/ms/rms/service/PaymentService.java
@@ -2,6 +2,7 @@ package com.solar.ms.rms.service;
 
 import com.solar.ms.rms.model.payment.ConfirmPaymentRequest;
 import com.solar.ms.rms.model.payment.ReservePaymentRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class PaymentService {
     @Autowired
@@ -28,20 +30,28 @@ public class PaymentService {
     private String linepayConfirmUrl;
 
     public ResponseEntity<?> reservePayment(ReservePaymentRequest reservePaymentRequest){
-        return loadBalancedRestTemplate.exchange(
+        ResponseEntity<String> response = loadBalancedRestTemplate.exchange(
                 linepayReserveUrl,
                 HttpMethod.POST,
-                new HttpEntity<>(new HttpHeaders()),
+                new HttpEntity<>(reservePaymentRequest, httpHeaders),
                 String.class
         );
+
+        log.info("RESERVE RESPONSE: {}", response.getBody());
+
+        return response;
     }
 
     public ResponseEntity<?> confirmPayment(ConfirmPaymentRequest confirmPaymentRequest){
-        return loadBalancedRestTemplate.exchange(
+        ResponseEntity<String> response = loadBalancedRestTemplate.exchange(
                 linepayConfirmUrl,
-                HttpMethod.GET,
-                new HttpEntity<>(new HttpHeaders()),
+                HttpMethod.POST,
+                new HttpEntity<>(confirmPaymentRequest, httpHeaders),
                 String.class
         );
+
+        log.info("CONFIRM RESPONSE: {}", response.getBody());
+
+        return response;
     }
 }
